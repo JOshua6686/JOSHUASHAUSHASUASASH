f6da0f0d4fe0f60a77191f19bd270a1dd234ee0c
JOshua6686 <msikaa6@hwbcymru.net>
2026-05-08 08:56:04 +0000

    jh

--- #Duck checker.py
+++ #Duck checker.py
@@ -1,10 +1,12 @@
 #Duck checker
 
+print("Welcome to the duck checker! This program will check if the word 'duck' is in a paragraph you provide.")
 para = input("PLease enter a paragraph: ")
 
 length = len(para)
 
-if "duck" in para:
+if str("duck") or "DUCK"  or "Duck" in para:
     print("The paragraph contains the word duck.")
 else:
     print("The paragraph does not contain the word duck.")
+    para.append("duck" * length)
